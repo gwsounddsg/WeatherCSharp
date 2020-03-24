fc8e89e0f0ea8dce62d550661e0a878589bf2df4
GW <gwsounddsg@gmail.com>
2020-03-24 10:49:38 -0400

    added json parser, printing full json sucessfully

--- Weather/WebCall.cs
+++ Weather/WebCall.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Text;
+using System.Runtime.Serialization;
+//using System.Runtime.Serialization.Json;
+
+using Newtonsoft.Json;
 
 
 
@@ -19,19 +22,42 @@ namespace Weather
             string myURL = url + zipCode + "&appid=" + Constants.WEATHER_KEY;
             
             // Create a request for the URL. 		
-            WebRequest request = WebRequest.Create(myURL);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(myURL);
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            Stream stream = response.GetResponseStream();
 
-            // If required by the server, set the credentials.
-            //request.Credentials = CredentialCache.DefaultCredentials;
+            // read and convert to json
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            string json = JsonConvert.SerializeObject(content);
 
-            // Get the response.
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // get temp data
+            JsonTextReader parser = new JsonTextReader(new StringReader(json));
+            while (parser.Read())
+            {
+                if (parser.Value != null)
+                {
+                    Console.WriteLine("Token: {0}, Value: {1}", parser.TokenType, parser.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Token: {0}", parser.TokenType);
+                }
+            }
 
-            // Display the status.
-            Console.WriteLine(response.StatusDescription);
-
-            // clean up
-            response.Close();
         }
+    }
+
+
+    public class InputGpioPort
+    {
+        [DataMember(Name = "pin")]
+        public string pin { get; set; }
+
+        [DataMember(Name = "gpio")]
+        public string gpio { get; set; }
+
+        [DataMember(Name = "value")]
+        public string value { get; set; }
     }
 }
